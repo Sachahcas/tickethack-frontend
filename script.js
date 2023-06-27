bed6992294f08f0216203eefd73f0cc1b6575ade
Thibaud <abonnel.t@gmail.com>
2023-06-27 18:58:49 +0200

    fix first add eventlistener

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.querySelector('#search').addEventListener('click', function () {
     const date = document.querySelector('#calendrier').value;
+    const departure = document.querySelector('#departureInput').value;
+    const arrival = document.querySelector('#arrivalInput').value;
 
-    console.log(date);
+    const travel ={date: date, departure: departure, arrival: arrival}
+
+    // console.log(travel);
 
     // POST ??????
 
     fetch('http://localhost:3000/trips', {
         method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ date }),
+		body: JSON.stringify({ travel }),
 	})
     .then(response => response.json())
 	.then(data => {console.log(data)});
